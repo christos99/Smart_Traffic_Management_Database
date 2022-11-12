8ebb05e4bd7240514d2f679c053c076fb6d40987
Christos Chrysikos <mangas.99@windowslive.com>
2022-11-12 14:55:20 +0200

    Delete smart-traffic-management-db-main directory

--- smart-traffic-management-db-main/queries/query2.sql
+++
@@ -1,5 +0,0 @@
--- names of areas with very low or low traffic metric
-
-SELECT Area.name AS 'areas with very low or low traffic'
-FROM Area
-WHERE Area.traffic_metric = 'VERY_LOW' OR Area.traffic_metric = 'LOW'
